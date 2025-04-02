b3e8e5ca78610768d29746534f6ef52ec89e9227
BBangsangbok <qwertyui9257@gmail.com>
2025-04-02 16:31:08 +0900

    comment Retrieve.py

--- data/Retrieve.py
+++ data/Retrieve.py
@@ -20,10 +20,17 @@ loaded_criteria_db = FAISS.load_local(
 
 _case_retriever = loaded_case_db.as_retriever(k=3)
 _criteria_retriever = loaded_criteria_db.as_retriever(k=10)
+
+
+#각각의 데이터베이스를 기반으로 한 retriever 반환
+#QA체인 등을 만들때 retriever 인자로 사용
 def get_case_retriever():
     return _case_retriever
 def get_criteria_retriever():
     return _criteria_retriever
 
+
+#각각의 데이터베이스에서 검색한 결과를 합치는 함수
+#반환값은 Document 형식
 def double_retrieve(query):
     return _case_retriever.invoke(query)+_criteria_retriever.invoke(query)
