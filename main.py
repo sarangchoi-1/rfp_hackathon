0e0db49c4638ed54f49431c03f1b313a6c9eecc2
xxeasty <greatshawn33@naver.com>
2025-04-02 14:52:34 +0900

    .

--- main.py
+++ main.py
@@ -4,14 +4,18 @@ from dotenv import load_dotenv
 import os
 import requests
 
-load_dotenv()
+from rag_pipeline.rag_pipeline import rag_pipeline
 
-filename = "YOUR_FILE_NAME"  # ex: ./image.png
+load_dotenv(dotenv_path="./config/.env")
+
+# filename = "YOUR_FILE_NAME"  # ex: ./image.png
  
-url = "https://api.upstage.ai/v1/document-digitization"
-headers = {"Authorization": f"Bearer {os.getenv('UPSTAGE_API_KEY')}"}
-files = {"document": open(filename, "rb")}
-data = {"ocr": "force", "base64_encoding": "['table']", "model": "document-parse"}
-response = requests.post(url, headers=headers, files=files, data=data)
- 
-print(response.json())
+# url = "https://api.upstage.ai/v1/document-digitization"
+# headers = {"Authorization": f"Bearer {os.getenv('UPSTAGE_API_KEY')}"}
+# files = {"document": open(filename, "rb")}
+# data = {"ocr": "force", "base64_encoding": "['table']", "model": "document-parse"}
+# response = requests.post(url, headers=headers, files=files, data=data)
+
+# print(response.json())
+
+print(rag_pipeline("팔협지가 누구야?"))
